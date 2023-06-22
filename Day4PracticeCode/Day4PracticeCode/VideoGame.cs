84a109c40ce39ead547b26abbb7e6c4992a86f04
JoshSaunders18 <122362331+JoshSaunders18@users.noreply.github.com>
2023-06-22 12:05:06 +0100

    Day4PracticeCode

---
+++ Day4PracticeCode/Day4PracticeCode/VideoGame.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day4PracticeCode
+{
+    public class VideoGame
+    {
+        public string NameOfVideoGame { get; set; }
+        public string TypeOfGame { get; set; }
+        
+        public string TypeOfGamingDevice { get; set; }
+
+        public int GameReleaseYear { get; set; }
+
+        public bool OnlineOrOffline { get; set; }   
+
+    }
+}
