0dfb83b9fbde09a7b131b0d03e51719042623df2
JoshSaunders18 <122362331+JoshSaunders18@users.noreply.github.com>
2023-06-22 11:03:05 +0100

    Csharpworkshop

---
+++ Day1Practice/Day1Practice/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+
+//creating another variable.
+
+string superHeroName = "The Flash";
+string secondMovieName = "Adipursh";
+
+//building the output 
+
+string outputString = "This Thursday, I will be watching the movie " + superHeroName;
+Console.WriteLine(outputString);
+string outputString2 = "This Friday, I will be watching the second movie" + secondMovieName;
+Console.WriteLine(outputString2);   
+
+Console.WriteLine(superHeroName);
+
+//create a variable to store an integer
+
+int myAge = 18;
+
+Console.WriteLine(myAge);
+
+//create a decimal variable
+
+decimal myMoney = 100.50m;
+
+Console.WriteLine(myMoney);
+
+//create a double variable
+
+Console.WriteLine(myDouble);
+
+//create a boolean variable 
+
+bool isitRaining = false;
+
+
+
